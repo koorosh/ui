730beecdadb23688719c2c09ffd188161b3e9938
Andrii Vorobiov <and.vorobiov@gmail.com>
2021-04-20 13:58:07 +0300

    cluster-ui: session page tracking events
    - bind analytics tracking for Back Button Click on session details page
    - add analytics tracking for statement click on session details page
    - code refactoring

--- packages/cluster-ui/src/sessions/sessionDetailsConnected.tsx
+++ packages/cluster-ui/src/sessions/sessionDetailsConnected.tsx
@@ -13,16 +13,11 @@ import { RouteComponentProps, withRouter } from "react-router-dom";
 import { analyticsActions, AppState } from "src/store";
 import { SessionDetails } from ".";
 import { actions as sessionsActions, selectSession } from "src/store/sessions";
-import {
-  actions as terminateQueryActions,
-  ICancelQueryRequest,
-  ICancelSessionRequest,
-} from "src/store/terminateQuery";
+import { actions as terminateQueryActions } from "src/store/terminateQuery";
 import { actions as nodesActions } from "src/store/nodes";
 import { actions as nodesLivenessActions } from "src/store/liveness";
 
 import { nodeDisplayNameByIDSelector } from "src/store/nodes";
-import { Dispatch } from "redux";
 
 export const SessionDetailsPageConnected = withRouter(
   connect(
@@ -31,32 +26,34 @@ export const SessionDetailsPageConnected = withRouter(
       session: selectSession(state, props),
       sessionError: state.adminUI.sessions.lastError,
     }),
-    (dispatch: Dispatch) => ({
-      refreshSessions: () => dispatch(sessionsActions.refresh()),
-      cancelSession: (payload: ICancelSessionRequest) =>
-        dispatch(terminateQueryActions.terminateSession(payload)),
-      cancelQuery: (payload: ICancelQueryRequest) =>
-        dispatch(terminateQueryActions.terminateQuery(payload)),
-      refreshNodes: () => dispatch(nodesActions.refresh()),
-      refreshNodesLiveness: () => dispatch(nodesLivenessActions.refresh()),
+    {
+      refreshSessions: sessionsActions.refresh,
+      cancelSession: terminateQueryActions.terminateSession,
+      cancelQuery: terminateQueryActions.terminateQuery,
+      refreshNodes: nodesActions.refresh,
+      refreshNodesLiveness: nodesLivenessActions.refresh,
+      onTerminateSessionClick: () =>
+        analyticsActions.track({
+          name: "Session Actions Clicked",
+          page: "Sessions Details",
+          action: "Terminate Session",
+        }),
+      onTerminateStatementClick: () =>
+        analyticsActions.track({
+          name: "Session Actions Clicked",
+          page: "Sessions Details",
+          action: "Terminate Statement",
+        }),
       onBackButtonClick: () =>
-        dispatch(
-          analyticsActions.track({
-            name: "Back Clicked",
-            page: "Sessions Details",
-          }),
-        ),
-      onSessionActionClicked: (
-        action: "Terminate Statement" | "Terminate Session",
-      ) => {
-        dispatch(
-          analyticsActions.track({
-            name: "Session Actions Clicked",
-            page: "Sessions",
-            action,
-          }),
-        );
-      },
-    }),
+        analyticsActions.track({
+          name: "Back Clicked",
+          page: "Sessions Details",
+        }),
+      onStatementClick: () =>
+        analyticsActions.track({
+          name: "Statement Clicked",
+          page: "Sessions Details",
+        }),
+    },
   )(SessionDetails),
 );
